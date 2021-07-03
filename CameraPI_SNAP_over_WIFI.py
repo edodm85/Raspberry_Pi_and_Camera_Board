b436e58d1e62d63a4a1177389a3b8864173a151a
edodm85 <edoardodemarchi@gmail.com>
2021-07-03 16:33:49 +0200

    Add files via upload

--- CameraPI_SNAP_over_WIFI.py
+++ CameraPI_SNAP_over_WIFI.py
@@ -6,12 +6,10 @@ import select
 import queue        #Python3
 
 
-
 width = 160
 height = 120
 format = "BaW"
 
-TCP_IP = '192.168.43.172'
 TCP_PORT = 2000
 BUFFER_SIZE = 1024
 BYTE_SEND = 9000
@@ -127,66 +125,107 @@ def parseCMD(cmd, conn):
     return str
 
 
+def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
 
 
-# TCP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((TCP_IP, TCP_PORT))
-server_socket.listen(1)
 
 
-inputs = [server_socket]
-outputs = []
-message_queues = {}
-
-while inputs:
-    readable, writable, exceptional = select.select(inputs, outputs, inputs)
-
-    for s in readable:
-        if s is server_socket:
-            connection, client_address = s.accept()
-            print("Connection address:", client_address)
-            # socket nonblocking
-            connection.setblocking(0)
-            inputs.append(connection)
-            message_queues[connection] = queue.Queue()
-        else:
-            data = s.recv(BUFFER_SIZE)
-            if data:
-                print("received data:", data)
-                dataRisp = parseCMD(data, s)
-
-                if len(dataRisp) > BYTE_SEND:
-                    data_split = np.array_split(dataRisp, (dataRisp.size / BYTE_SEND))
-                    for data in data_split:
-                        message_queues[s].put(data)
-                else:
-                    message_queues[s].put(dataRisp)
-                if s not in outputs:
-                    outputs.append(s)
-            else:
-                if s in outputs:
-                    outputs.remove(s)
-                inputs.remove(s)
-                s.close()
-                print("Connection disconnect")
-                camera.stop_preview()
-                del message_queues[s]
-
-    for s in writable:
-        try:
-            next_msg = message_queues[s].get_nowait()
-        except queue.Empty:
-            outputs.remove(s)
-        else:
-            s.send(next_msg)
-
-    for s in exceptional:
-        inputs.remove(s)
-        if s in outputs:
-            outputs.remove(s)
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
         s.close()
-        print("Connection disconnect")
-        camera.stop_preview()
-        del message_queues[s]
+    return IP
+
+
+# MAIN TCP socket
+if __name__ == "__main__":
+    # Create an INET (Internet Protocol v4 addresses), STREAMing socket (SOCK_STREAM for TCP and SOCK_DGRAM for UDP)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Make socket reusable
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    IP = get_ip()
+    print("Starting up on %s port %s" % (IP, TCP_PORT))
+    server_socket.bind((IP, TCP_PORT))
+
+    # Listen for incoming connections
+    # The integer argument is the number of connections the system should queue up in the background before rejecting new clients.
+    server_socket.listen(1)
+
+
+    inputs = [server_socket]
+    outputs = []
+    message_queues = {}
+
+    while inputs:
+        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+
+        for s in readable:
+            if s is server_socket:
+                # accept() returns an open connection between the server and client
+                connection, client_address = s.accept()
+                print("Connection address:", client_address)
+
+                set_keepalive_linux(connection)
+
+                # socket nonblocking
+                connection.setblocking(0)
+                inputs.append(connection)
+                message_queues[connection] = queue.Queue()
+            else:
+                data = s.recv(BUFFER_SIZE)
+                if data:
+                    print("received data:", data)
+                    dataRisp = parseCMD(data, s)
+
+                    if len(dataRisp) > BYTE_SEND:
+                        data_split = np.array_split(dataRisp, (dataRisp.size / BYTE_SEND))
+                        for data in data_split:
+                            message_queues[s].put(data)
+                    else:
+                        message_queues[s].put(dataRisp)
+                    if s not in outputs:
+                        outputs.append(s)
+                else:
+                    if s in outputs:
+                        outputs.remove(s)
+                    inputs.remove(s)
+                    s.close()
+                    print("Connection disconnect")
+                    camera.stop_preview()
+                    del message_queues[s]
+
+        for s in writable:
+            try:
+                next_msg = message_queues[s].get_nowait()
+            except queue.Empty:
+                outputs.remove(s)
+            else:
+                s.send(next_msg)
+
+        for s in exceptional:
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
+            print("Connection disconnect ex")
+            camera.stop_preview()
+            del message_queues[s]
 
